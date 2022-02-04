ad9e75e2bea71ac89452c9599287a23bf4a928d7
onesketchyguy <lowebros9812@gmail.com>
2022-02-04 00:32:24 -0800

    Added more packages and time manager

---
+++ Scripts/TimeScaleManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public static class TimeScaleManager
+{
+    private static float timeScale = -1;
+    private static float fixedTimeScale = -1;
+
+    private static float timeScaleMultiplier;
+
+    private static void Initialize()
+    {
+        fixedTimeScale = Time.fixedDeltaTime;
+        timeScale = Time.timeScale;
+
+        timeScaleMultiplier = 1;
+    }
+
+    public static float GetTimeScale() => timeScaleMultiplier;
+
+    public static void SetTimeScale(float scale)
+    {
+        if (fixedTimeScale == -1) Initialize();
+
+        timeScaleMultiplier = scale;
+
+        Time.fixedDeltaTime = fixedTimeScale * timeScaleMultiplier;
+        Time.timeScale = timeScale * timeScaleMultiplier;
+    }
+}
